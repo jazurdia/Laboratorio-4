357ae258805c5304490d6ae8931c49ecd900a204
Diego2250 <mor21146@uvg.edu.gt>
2021-11-15 10:26:26 -0600

    Update Vista.java

--- Principal/Vista.java
+++ Principal/Vista.java
@@ -1,11 +1,66 @@
 package Principal;
+/***************************************************
+Vista.java
+Autor: Diego Morales, Javier Azurdia
+Fecha: 15/11/2021
 
-public class Vista {
-    public Vista(){
-    }
-
+Vista. Responsable de toda interaccion con el usuario.
+***************************************************/
+import java.util.Scanner;
+public class vista{
+	private Scanner scan;
+    /*Costructor de clase*/
+	public vista(){
+		scan = new Scanner(System.in);
+	}
     public void show(String txt){
         System.out.println(txt);
     }
+
+     /*Metodo para elegír una opción
+  @return número de opción
+  */
+	public int menuOpciones(){
+		int op=0;
+				try{
+			String s = "¿Que deseas hacer?\n" +
+					"1. \n" +
+					"2. \n" +
+					"3. \n" +
+                    "4. \n" +
+					"5. Salir\n";
+
+		System.out.println(s);
+		op = scan.nextInt();
+	}catch (Exception e){
+				error();
+				scan.next();
+		}
+		return op;
+	}
+
+    /*Metodo para mostrar mensaje de despedida al usuario
+	*/
+	public void despedida(){
+		System.out.println("Gracias por usar este programa, adios");
+	}
+	/*Metodo para mostrar mensaje de bienvenida al usuario
+	*/
+	public void bienvenida(){
+		System.out.println("¡Bienvendio(a) a QueOndaMano!");
+	}
+
+	/*Metodo para mostrar mensaje
+	*/
+	public void opcionInvalida(){
+		System.out.println("Has elegido una opción inválida.");
+	}
+
+	/*Metodo para mostrar mensaje
+	*/
+    public void error(){
+        System.out.println("Ha ocurrido un error");
+      }
+
 
 }
